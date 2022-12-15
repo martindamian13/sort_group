f064288f8c5de3f2473676057e6845b7617dff51
martindamian13 <cachobnitz@icloud.com>
2022-12-15 10:53:35 -0300

    updated database

--- static/script.js
+++ static/script.js
@@ -17,7 +17,7 @@ function appendData(data) {
     out += `
     <tr>
     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">${data[d].nombre}</td>
-    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><div class="w-16 h-12"><img id="imagen${data[d].id}" src="" alt=""></div></td>
+    <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"><img class="w-20" id="imagen${data[d].id}" src="" alt=""></td>
     <td class="grid justify-items-stretch whitespace-nowrap px-3 py-4"><button onclick="changeImage(${data[d].id})" type="button" class="justify-self-end rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium text-white shadow-sm">Sortear</button></td>
     </tr>
     `;
@@ -29,7 +29,7 @@ function appendData(data) {
 function changeImage(id) {
   var image = document.getElementById('imagen'+id);
   if (image.src.match("")) {
-    image.src = "https://media.tenor.com/UnFx-k_lSckAAAAM/amalie-steiness.gif"
+    image.src = "https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921"
     setTimeout(function(){grupo(image, id)}, 2000);
   }
 }
@@ -44,13 +44,13 @@ function grupo(image, id){
     if (data[d].id == id){
       console.log (data[d].grupo)
       if (data[d].grupo == "1"){
-        image.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/311cf017-3c0d-467c-bce6-5fd427c47c92/de9tt6p-3839abd4-605f-42a4-b26a-f5c415e59df4.png"
+        image.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ed991cf4-7c8c-4530-b6ba-a3abf3ab2eae/ddy76r7-0da8b48d-a7e7-4464-a433-a51bd214598c.png/v1/fill/w_1280,h_696,strp/power_rangers_mystic_force_logo_by_joshuat1306_ddy76r7-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Njk2IiwicGF0aCI6IlwvZlwvZWQ5OTFjZjQtN2M4Yy00NTMwLWI2YmEtYTNhYmYzYWIyZWFlXC9kZHk3NnI3LTBkYThiNDhkLWE3ZTctNDQ2NC1hNDMzLWE1MWJkMjE0NTk4Yy5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.AhbzzW6sq2KDQ2S2sDCexWOiL8FJn11fxwiMiVPCFc4"
       }
       else if (data[d].grupo == "2"){
         image.src = "https://static.wikia.nocookie.net/logopedia/images/c/ca/Saban%C2%B4s_Power_Rangers_Wild_Force_Logo.png"
       }
       else if (data[d].grupo == 3){
-        image.src = "https://static.wikia.nocookie.net/logopedia/images/c/ca/Saban%C2%B4s_Power_Rangers_Wild_Force_Logo.png"
+        image.src = "https://www.pngitem.com/pimgs/m/522-5223937_dino-charge-logo-v2-by-joeshiba-on-deviantart.png"
       }
     }
   }
